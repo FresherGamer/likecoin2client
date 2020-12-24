54e675e540edf793faf4449370c0ed4bfb32a11c
FresherGamer <freshergamer@yandex.ru>
2020-12-24 17:46:23 +0300

    Updates

---
+++ likecoin2client/precache-manifest.fb1e3d7d325531174e2bdd87cbfbd592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c7f9220a3f5f32b04d7dd4847c65c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4df12a9ccc1c66d8d6",
+    "url": "/static/css/main.69d3d7d9.chunk.css"
+  },
+  {
+    "revision": "23d65430f0d9c9f124d5",
+    "url": "/static/js/2.a8edbf0b.chunk.js"
+  },
+  {
+    "revision": "aa49abf78f7f108efe22bc9ef1360602",
+    "url": "/static/js/2.a8edbf0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4df12a9ccc1c66d8d6",
+    "url": "/static/js/main.2ec4b9dd.chunk.js"
+  },
+  {
+    "revision": "df1119aedde00fec4337",
+    "url": "/static/js/runtime-main.44423184.js"
+  }
+]);
