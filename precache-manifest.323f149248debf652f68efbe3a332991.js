64c3a0996c03c4714377550fa704d01814240fa8
FresherGamer <freshergamer@yandex.ru>
2020-12-24 14:39:45 +0300

    Updates

---
+++ precache-manifest.323f149248debf652f68efbe3a332991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23cec2cbe1613dc01be8d42a27035d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62e8e0e0d20dfd2b490",
+    "url": "/static/css/main.69d3d7d9.chunk.css"
+  },
+  {
+    "revision": "23d65430f0d9c9f124d5",
+    "url": "/static/js/2.a8edbf0b.chunk.js"
+  },
+  {
+    "revision": "aa49abf78f7f108efe22bc9ef1360602",
+    "url": "/static/js/2.a8edbf0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62e8e0e0d20dfd2b490",
+    "url": "/static/js/main.2a61973c.chunk.js"
+  },
+  {
+    "revision": "df1119aedde00fec4337",
+    "url": "/static/js/runtime-main.44423184.js"
+  }
+]);
