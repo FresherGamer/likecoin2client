6d144e84cc37a519cbf6dfe73984b9cb5a6d5965
FresherGamer <freshergamer@yandex.ru>
2020-12-29 19:51:21 +0300

    Updates

---
+++ precache-manifest.267df23bb6c53fd3c51e6c2085a557ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5738d73a10a91e29f41c5364d1c2c35e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22857a4298d211a2aa7",
+    "url": "/static/css/main.69d3d7d9.chunk.css"
+  },
+  {
+    "revision": "cddfcc8c51fd6de105fa",
+    "url": "/static/js/2.0c21fe0d.chunk.js"
+  },
+  {
+    "revision": "aa49abf78f7f108efe22bc9ef1360602",
+    "url": "/static/js/2.0c21fe0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22857a4298d211a2aa7",
+    "url": "/static/js/main.ffbef393.chunk.js"
+  },
+  {
+    "revision": "df1119aedde00fec4337",
+    "url": "/static/js/runtime-main.44423184.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/likecoin2client/precache-manifest.af30d7e388c061bb36287162ab075a14.js"
+  "/precache-manifest.267df23bb6c53fd3c51e6c2085a557ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/likecoin2client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
